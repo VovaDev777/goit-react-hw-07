196b853797a63f9ee07151121393a6e9d8095a7f
VovaDev777 <polvovater@gmail.com>
2024-08-03 22:50:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -7,3 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
