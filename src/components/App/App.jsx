f5e4a906e16d5a8454a9eaa9e012108063c877ff
VovaDev777 <polvovater@gmail.com>
2024-08-06 15:05:06 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import ContactForm from "../ContactForm/ContactForm"
 import SearchBox from '../SearchBox/SearchBox'
 import ContactList from "../ContactList/ContactList";
+import Loader from "../Loader/Loader";
 import css from './App.module.css'
 import '../../reset.css'
 import { useEffect } from "react";
@@ -10,7 +11,7 @@ import { useDispatch } from "react-redux";
 
 const App = () => {
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch])
@@ -20,6 +21,7 @@ const App = () => {
       <h1 className={css.text}>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      <Loader />
       <ContactList />
     </div>
   )
