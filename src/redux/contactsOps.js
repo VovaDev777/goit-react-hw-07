37f199998cece7c11e287c07da160852bb24f0db
VovaDev777 <polvovater@gmail.com>
2024-08-06 14:26:07 +0200

    Update contactsOps.js

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://66b0cad06a693a95b53a3af1.mockapi.io";
 
-export const fetchContacts = createAsyncThunk("base-action", async () => {
-   try {
-    const response = await axios.get("/contacts");
-    return response.data;
-   } catch (error) {
-        
-   }
+export const fetchContacts = createAsyncThunk("getContacts", async (_, thunkAPI) => {
+    try {
+        const response = await axios.get("/contacts");
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
 })
